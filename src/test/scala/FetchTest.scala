305f037412029207febd626045096e0a24f1bd72
watasuke102 <watasuke102@gmail.com>
2022-07-20 01:13:56 +0000

    2-5: fetch instructions

---
+++ src/test/scala/FetchTest.scala
@@ -0,0 +1,16 @@
+package fetch
+
+import chisel3._
+import org.scalatest._
+import chiseltest._
+
+class HexTest extends FlatSpec with ChiselScalatestTester {
+  "mycpu" should "work through hex" in {
+    // c: instance of Top class
+    test(new Top) { c =>
+      while (!c.io.exit.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+    }
+  }
+}
